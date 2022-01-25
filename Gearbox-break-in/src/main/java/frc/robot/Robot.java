12cc4b97f2ec9a927b382947bb689f34033fc6ca
Richard Buckley <42151145+rhbuckley@users.noreply.github.com>
2022-01-25 16:32:06 -0500

    Update Robot.java
    Seems to run at first glance. Need to incorporate getting the SendableChooser's value to switch between AUTO and MANUAL.

--- Gearbox-break-in/src/main/java/frc/robot/Robot.java
+++ Gearbox-break-in/src/main/java/frc/robot/Robot.java
@@ -1,14 +1,10 @@
 package frc.robot;
 
-import java.util.concurrent.TimeUnit;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
 
@@ -19,22 +15,27 @@ public class Robot extends TimedRobot {
   private int m_motor2_port = 1; 
 
   private double m_seconds = 0;
-  private double m_desiredSpeed = 0.0;
+  private double m_seconds_periodic = 0; 
+  private double m_seconds_remaining = m_seconds; 
+  private boolean m_reverse = false;
 
-  private double m_num_repitions = 0; // If 0, repetitions is infinite
-  private XboxController xbox; 
-
+  
+  
   // Smartdashboard Values
-  private double m_speed = 0.4;
-  private int m_repetitions = 15;
+  private boolean m_auto = false;
+  private double m_RampUp = 5; 
+  private double m_speed = 0.0;
+  private int m_repetitions = 0;
   private int m_timeToReverse = 15;
 
+  // Constants
+  private double kFrequency = 0.002;
 
 
   public Robot() {
     motor1 = new WPI_TalonFX(m_motor1_port);
     motor2 = new WPI_TalonFX(m_motor2_port);
-    xbox = new XboxController(1);
+
   }
   
   private void spin(double speed) {
@@ -42,71 +43,78 @@ public class Robot extends TimedRobot {
     motor2.set(-speed);
   }
 
-  private void breakIn() {
-    // The frequency of this being called is every 0.2 seconds
-    // so increment by 0.2
-    m_repetitions+=1;
-    if (m_repetitions != m_num_repitions) {
-      if (m_timeToReverse == m_seconds) {
-        m_speed=-m_speed;
-        m_seconds=0;
-      }
-    spin(m_speed);
-    m_seconds += .002; 
-  }}
-  
-  @Override
-  public void teleopInit() {
-    m_desiredSpeed = 0.0;
-    spin(m_desiredSpeed);
+  private void onDataChange() {
+    m_seconds = SmartDashboard.getNumber("Seconds", m_seconds);
+    m_speed = SmartDashboard.getNumber("Speed", m_speed);
+    if (m_speed > 1) {m_speed = m_speed/100;}
+    m_RampUp = SmartDashboard.getNumber("Ramp Up", m_RampUp);
+    /// TODO: get values from SENDABLE CHOOSER
   }
 
+  private double getSpeed() {
+    double speed = m_speed;
+    // RAMP UP
+    if (m_seconds < m_RampUp) {
+      return (speed/m_RampUp)/m_seconds;
+    }
+
+    // REVERSING GEAR BOX
+    if (m_seconds_periodic == m_timeToReverse) {
+      if (m_reverse) {
+        m_reverse = false;
+      } else {
+        m_reverse = true; 
+      }
+      m_seconds_periodic = 0; 
+    }
+
+    if (m_reverse) {
+    return -speed;
+    } else {
+      return speed;
+    }
+  }
+
+  @Override
+  public void teleopInit() {
+    m_speed = 0.0;
+    spin(m_speed);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    onDataChange();
+    if (m_auto) {
+      m_repetitions+=1;
+      m_seconds_periodic += kFrequency; 
+      m_seconds_remaining -= kFrequency; 
+      SmartDashboard.updateValues();
+      spin(getSpeed());
+    } else {
+      spin(m_speed);
+    }
+  }
   
   @Override
   public void robotInit() {
-  //  SendableChooser<Command> m_chooser = new SendableChooser<>();
+    SendableChooser<Boolean> auto = new SendableChooser<Boolean>();
+    auto.addOption("Auto", true);
+    auto.addOption("Manual", false);
+    auto.setDefaultOption("Auto", true);
+    SmartDashboard.putData(auto);
+    SmartDashboard.putBoolean("Auto", true); // CHANGEABLE
+    SmartDashboard.putNumber("Seconds Remaining", m_seconds_remaining); // STATIC
+    SmartDashboard.putNumber("Seconds", m_seconds); // CHANGEABLE
+    SmartDashboard.putNumber("Speed", m_speed); // CHANGEABLE
+    SmartDashboard.putNumber("Ramp Up", m_RampUp); // CHANGEABLE
+    SmartDashboard.putNumber("Repetitions", m_repetitions); // STATIC
+    SmartDashboard.putBoolean("Reverse?", m_reverse); // STATIC
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    if (xbox.getAButtonPressed()){
-      m_speed += 0.1;
-      System.out.println(m_speed);
-    }
-
-    if (xbox.getBButtonPressed()){
-      m_speed -= 0.1;
-      System.out.println(m_speed);
-    }
-
-    if (xbox.getXButtonPressed()){
-      breakIn();
-    }
-    if (xbox.getYButtonPressed()){
-      m_speed = 0.0;
-    }
-
     m_speed = Math.min(1.0, Math.max(-1.0, m_speed));
-    //CommandScheduler.getInstance().run();
   }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
 
 
 
